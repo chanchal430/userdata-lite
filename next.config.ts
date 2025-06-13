2c06b43614b725fbfd41a6131a0e77161cde063d
chanchal430 <kchanchal430@gmail.com>
2025-06-13 20:33:46 +0530

    Fix Telegram SDK usage for Vercel build

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   swcMinify: true,
-  output: "export",
+  // output: "export",
 };
 
 export default nextConfig;
