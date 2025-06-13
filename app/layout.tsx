fd418dca10d9bb098b8a458f2bf61c582b49887c
chanchal430 <kchanchal430@gmail.com>
2025-06-13 19:12:24 +0530

    all files for demo

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Telegram Mini App",
+  description: "A simple Telegram Mini App created with Next js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        {/* ✅ Load Telegram WebApp script asynchronously */}
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js?57"
+          strategy="beforeInteractive"
+        />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import webApp from "@twa-dev/sdk";
+
+interface UserData {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  userName?: string;
+  language_code: string;
+  is_premium: boolean;
+}
+
+export default function Home() {
+  const [userData, setUserData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    if (webApp.initDataUnsafe.user) {
+      setUserData(webApp.initDataUnsafe.user as UserData);
+    }
+  }, []);
+  return (
+    <main className="p-4">
+      {userData ? (
+        <>
+          <h1 className="text-2xl font-bold mb-4">User Data</h1>
+          <ul>
+            <li>ID: {userData.id} </li>
+            <li>First Name: {userData.first_name}</li>
+            <li>Last Name: {userData.last_name}</li>
+            <li>UserName: {userData.userName}</li>
+            <li>Language Code: {userData.language_code}</li>
+            <li>Is Premium: {userData.is_premium ? "Yes" : "No"}</li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <div>Loading...</div>
+        </>
+      )}
+    </main>
+  );
+}
